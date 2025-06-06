60940933920caccbdc18cc0e01d340c827aba431
yuzaku <yanuaraudrey55@gmail.com>
2025-06-06 15:13:09 +0700

    update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
 
+console.log("Middleware running");
+
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
