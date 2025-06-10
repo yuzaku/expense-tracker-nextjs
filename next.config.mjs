1648fc9e3b1702bc0a957941ab1693aaee743b9d
yuzaku <yanuaraudrey55@gmail.com>
2025-06-10 09:56:40 +0700

    revert changes next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'standalone',
-    experimental: {
-        serverComponentsExternalPackages: ['@clerk/nextjs']
-    },
-    env: {
-        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
