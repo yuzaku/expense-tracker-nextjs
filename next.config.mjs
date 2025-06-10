b3a9d2040b167efd53cd4f7ea714f26941580a0c
yuzaku <yanuaraudrey55@gmail.com>
2025-06-10 09:51:35 +0700

    update: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone',
+    experimental: {
+        serverComponentsExternalPackages: ['@clerk/nextjs']
+    },
+    env: {
+        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    }
+};
 
 export default nextConfig;
